b206a512dc2d016628b10c489f3a14e32d208386
youngsing <youngsingyx@gmail.com>
2016-12-02 17:15:37 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.get('*', function (req, res) {
+	console.log(req.headers);
+	
+	var result = {};
+	
+	var sys = req.headers['user-agent'];
+	sys = sys.match(/\((.+?)\)/)[1];
+	result['software'] = sys;
+	
+	var lng = req.headers['accept-language'];
+	lng = lng.split(';')[0].split(",")[0];
+	result['language'] = lng;
+	
+	var ip = req.headers['x-forwarded-for'];
+	result['idaddress'] = ip;
+	
+	res.json(result);
+});
+
+app.listen(8080, function () {
+	console.log("Listening on port 8080!");
+});
